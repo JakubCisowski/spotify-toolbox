225be1b26edda24ee035144d667db7cbfa14ff58
JakubCisowski <jakub.cisowski1@gmail.com>
2022-08-13 15:30:31 +0200

    Add HTML for tutorial section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
+  const [playlistLink, setPlaylistLink] = useState('');
+
   const { data, isLoading } = trpc.useQuery([
     'example.hello',
     { text: 'from tRPC' },
@@ -21,7 +25,11 @@ const Home: NextPage = () => {
 
       <div>
         <Logo />
-        <PlaylistSearchbar />
+        <PlaylistSearchbar
+          playlistLink={playlistLink}
+          onPlaylistLinkChange={setPlaylistLink}
+        />
+        <TutorialSection />
       </div>
     </>
   );
@@ -30,12 +38,22 @@ const Home: NextPage = () => {
 function Logo() {
   return (
     <>
-      <h2> spotify toolbox • find playlist genre </h2>
+      <Link href="/">
+        <a className="logo-text">
+          <span className="green">spotify</span> toolbox • find playlist genre
+        </a>
+      </Link>
     </>
   );
 }
 
-function PlaylistSearchbar() {
+function PlaylistSearchbar({
+  playlistLink,
+  onPlaylistLinkChange,
+}: {
+  playlistLink: string;
+  onPlaylistLinkChange: (playlistLink: string) => void;
+}) {
   return (
     <>
       <form action="/playlist-genre" method="get">
@@ -43,10 +61,72 @@ function PlaylistSearchbar() {
           type="text"
           name="link"
           placeholder="paste spotify playlist link"
-          value=""
+          value={playlistLink}
+          onChange={(e) => onPlaylistLinkChange(e.target.value)}
+          className="input-text-style"
         />
-        <input type="submit" value="analyze" />
+        <input type="submit" value="analyze" className="input-submit-style" />
       </form>
+    </>
+  );
+}
+
+function TutorialSection() {
+  return (
+    <>
+      <h3>how it works?</h3>
+      <p>1) copy spotify playlist link</p>
+      <Image
+        src="https://i.imgur.com/qyY8xR4.gif"
+        alt="tutorial1"
+        className="how-it-works-resource"
+        width={847}
+        height={631}
+      ></Image>
+      <p>2) paste it and click analyze</p>
+      <Image
+        src="https://i.imgur.com/XqQUuMo.gif"
+        alt="tutorial2"
+        className="how-it-works-resource"
+        width={1142}
+        height={137}
+      ></Image>
+      <p> 3) wait for the analysis to finish </p>
+      <Image
+        src="https://i.imgur.com/UnSukGJ.gif"
+        alt="tutorial3"
+        className="how-it-works-resource"
+        width={425}
+        height={162}
+      ></Image>
+      <p> 4) check analysis result </p>
+      <Image
+        src="https://i.imgur.com/JBV1uiX.png"
+        alt="tutorial4"
+        className="how-it-works-resource"
+        width={422}
+        height={694}
+      ></Image>
+      <p>
+        <span className="green">
+          <b>70</b>
+          <span className="percentage">% </span>
+        </span>
+        this means that 70% of the tracks* in this playlist are classified by
+        Spotify as a certain genre of music
+      </p>
+      <p>
+        some tracks are <span className="red">not classified</span>, it&apos;s
+        indicated at the botom of analysis results
+      </p>
+      <p> --- </p>
+      <p className="side-note">
+        <em>
+          * actually Spotify classifies artists rather than tracks, so to be
+          precise 70% means that 70% of the tracks in this playlist were created
+          by artists related to the genre
+        </em>
+      </p>
     </>
   );
 }
